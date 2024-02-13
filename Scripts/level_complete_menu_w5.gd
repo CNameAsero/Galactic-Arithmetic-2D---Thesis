528bc9b57458062ee9e276f37ff8654187b0b0a6
dariusmark23 <cruzdariusmark@gmail.com>
2024-02-13 11:06:56 +0800

    last for this morning

--- Scripts/level_complete_menu_w5.gd
+++ Scripts/level_complete_menu_w5.gd
@@ -8,10 +8,11 @@ func _on_next_level_button_pressed():
 	AudioManager.play_button_sfx()
 	var cut_scene_path = "res://Scenes/Menu/Main_Menu/finalscene.tscn"
 	var next_scene_path = "res://Scenes/levels/level_" + str(GameSettings.current_level+1) + ".tscn"
-	if GameSettings.current_level % 5 == 0:
+	if GameSettings.current_level % 25 == 0:
 		if ResourceLoader.exists(cut_scene_path):
 			AudioManager.level5_music.stop()
 			get_tree().change_scene_to_file(cut_scene_path)
+			print("qweqe")
 	else:
 		if ResourceLoader.exists(next_scene_path):
 			get_tree().change_scene_to_file(next_scene_path)
