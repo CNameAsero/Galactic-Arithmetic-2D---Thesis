b2635989645de3ce842b4b3591b9a613b6a5db3d
dariusmark23 <cruzdariusmark@gmail.com>
2024-02-13 09:01:42 +0800

    projectile fixed

--- Scripts/bullets.gd
+++ Scripts/bullets.gd
@@ -4,9 +4,12 @@ var direction : Vector2 = Vector2.RIGHT
 @export var speed : float = 300
 
 @onready var health_system = $"../health_system"
+@onready var sprite1 = $"../slime_player_joystick/slime_player_joystik/Sprite2D"
 
 func _ready():
 	await get_tree().create_timer(3).timeout
+	sprite1.visible = true
+	sprite1.modulate = Color(1, 1, 1, 1)
 	queue_free()
 
 func _physics_process(delta):
--- Scripts/fly_bullet.gd
+++ Scripts/fly_bullet.gd
@@ -3,9 +3,12 @@ extends Area2D
 var direction : Vector2 = Vector2.RIGHT
 @export var speed : float = 300
 @onready var health_system = $"../health_system"
+@onready var sprite1 = $"../slime_player_joystick/slime_player_joystik/Sprite2D"
 
 func _ready():
 	await get_tree().create_timer(1.5).timeout
+	sprite1.visible = true
+	sprite1.modulate = Color(1, 1, 1, 1)
 	queue_free()
 
 func _physics_process(delta):
